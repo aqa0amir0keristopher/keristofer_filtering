1bb4d575b0776e52132c77ad02337b8d043ebdc5
aqa0amir0keristopher <98421818+aqa0amir0keristopher@users.noreply.github.com>
2022-01-25 22:41:39 +0330

    Create filtering-keristofer.py

---
+++ filtering-keristofer.py
@@ -0,0 +1,79 @@
+print("aqa amir")
+print("keristofer")
+import os
+#What do you have in the source?
+os.system("pip install colored")
+os.system("pip install colorama")
+os.system("pip install datetime")
+#importing
+import time
+import sys
+import colored
+import colorama
+import datetime
+from colored import fg, bg, attr
+from colorama import Fore, Back, Style
+# colors
+red='\033[31m'
+green='\033[32m'
+blue='\033[36m'
+pink='\033[35m'
+rang='\033[34m'
+os.system("clear")
+print(f"{blue} ")
+print("""
+	
+	     [ (aqa amir (keristofer) ]
+	     	
+	     	
+	
+	     .o oOOOOOOOo                                            OOOo
+             Ob.OOOOOOOo  OOOo.      oOOo.                      .adOOOOOOO
+             OboO''''''''''''.OOo. .oOOOOOo.    OOOo.oOOOOOo..''''''''OO
+             OOP.oOOOOOOOOOOO "POOOOOOOOOOOo.   `"OOOOOOOOOP,OOOOOOOOOOOB'
+             `O'OOOO'     `OOOOo"OOOOOOOOOOO` .adOOOOOOOOO"oOOO'    `OOOOo
+             .OOOO'            `OOOOOOOOOOOOOOOOOOOOOOOOOO'            `OO
+             OOOOO                 '"OOOOOOOOOOOOOOOO"`                oOO
+            oOOOOOba.                .adOOOOOOOOOOba               .adOOOOo.
+           oOOOOOOOOOOOOOba.    .adOOOOOOOOOO@^OOOOOOOba.     .adOOOOOOOOOOOO
+          OOOOOOOOOOOOOOOOO.OOOOOOOOOOOOOO"`  '"OOOOOOOOOOOOO.OOOOOOOOOOOOOO
+          "OOOO"       "YOoOOOOMOIONODOO"`  .   '"OOROAOPOEOOOoOY"     "OOO"
+             Y           'OOOOOOOOOOOOOO: .oOOo. :OOOOOOOOOOO?'         :`
+             :            .oO%OOOOOOOOOOo.OOOOOO.oOOOOOOOOOOOO?
+                          oOOP"%OOOOOOOOoOOOOOOO?oOOOOO?OOOO"OOo
+                          '%o  OOOO"%OOOO%"%OOOOO"OOOOOO"OOO':
+                               `$"  `OOOO' `O"Y ' `OOOO'  o
+                                      OP"          : o
+                                     
+                                      
+""")
+print(f"{pink} ")
+print(" ")
+up = input("""
+	
+[ Di script 1 ]
+
+[ Super link 2 ]
+
+Which do you want > """)
+if up == "1":
+            print(' Di script == %aa% https://dxript.com fg(f"{pink} ").bg(f"{blue} ").attr(f"{green} ")(reste1.8)yfttk15 xexxxx')
+if up == "2":
+            print(' link == https://s4.uupload.ir/files/img_20210728_194736_412_9t6h.jpg')
+print(f"{green} ")
+print("     aqa   amir    ")
+print("     keristofer    ")
+print("""
+                id chanel rubika = @aqa_Christopher
+                
+                
+                
+                bay
+
+
+
+                 
+
+
+
+""")
